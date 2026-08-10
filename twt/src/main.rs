6243623646066670cb529355d4e9d76177adc81c
Lucky Jha <jhalucky61@gmail.com>
2026-08-10 11:59:56 +0530

    chore: Day1 variables and datatypes

--- twt/src/main.rs
+++ twt/src/main.rs
@@ -1,4 +1,16 @@
 fn main() {
-    let x = 4;
+    // we use "mut" keyword to make any variable mutable because bydefault all variables declared in rust are immutable
+    let mut x = 4;
     println!("x is: {}",x);
+
+    x = 5;
+    println!("x is: {}",x);
+
+    let mut y = "raveesh";
+    println!("y is: {}",y);
+
+    y = "Raveesh";
+    println!("y is: {}",y);
 }
+
+
