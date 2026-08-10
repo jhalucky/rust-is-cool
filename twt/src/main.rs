02e8028503d978fd987df71cddfdc3ebc548bf1e
Lucky Jha <jhalucky61@gmail.com>
2026-08-10 14:23:11 +0530

    chore: let and const

--- twt/src/main.rs
+++ twt/src/main.rs
@@ -1,16 +1,25 @@
+// fn main() {
+//     // we use "mut" keyword to make any variable mutable because bydefault all variables declared in rust are immutable
+//     let mut x = 4;
+//     println!("x is: {}",x);
+
+//     x = 5;
+//     println!("x is: {}",x);
+
+//     let mut y = "raveesh";
+//     // println!("y is: {}",y);
+
+//     y = "Raveesh";
+//     println!("y is: {}",y);
+
+// }
+
+
 fn main() {
-    // we use "mut" keyword to make any variable mutable because bydefault all variables declared in rust are immutable
-    let mut x = 4;
-    println!("x is: {}",x);
+    const NEW_VAR: u32 = 60;
+    println!("{}", NEW_VAR);
 
-    x = 5;
-    println!("x is: {}",x);
-
-    let mut y = "raveesh";
-    println!("y is: {}",y);
-
-    y = "Raveesh";
-    println!("y is: {}",y);
+    const NEW_STR: str =  "Gulati";
+    println!("{}",NEW_STR)
 }
-
 
