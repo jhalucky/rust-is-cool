a122f0c1417c63325bc39534c3e345548743a329
Lucky Jha <jhalucky61@gmail.com>
2026-08-13 12:19:01 +0530

    chore: compound conditionals

--- twt/examples/conditional.rs
+++ twt/examples/conditional.rs
@@ -2,6 +2,23 @@ fn main() {
     let num1 = (20 as u8);
     let num2: u8 = 20;
 
-    let cond = num1 >= num2;
-    println!("{}",cond);
+    let cond = num1 != num2;
+
+    // compound conditions
+
+    let cond2 = false || !cond;
+
+    // println!("{}",cond);
+    // println!("{}",cond2);
+    control_flow();
+}
+
+fn control_flow() {
+    let food = "cookie";
+
+    if food != "cookie" {
+        println!("I love cookie.");
+    } else {
+        println!("Where are cookies?");
+    }
 }
