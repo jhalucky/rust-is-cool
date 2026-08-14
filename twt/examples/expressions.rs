74f9acbfa05c97f40c47fc13aec406bb25371459
Lucky Jha <jhalucky61@gmail.com>
2026-08-14 13:42:06 +0530

    chore: function return

--- twt/examples/expressions.rs
+++ twt/examples/expressions.rs
@@ -1,10 +1,25 @@
 fn main() {
     println!("Hello Rust!");
 
-    let number = {
-        let x = 3;
-        x+3
-    };
+    // let number = {
+    //     let x = 3;
+    //     x+3
+    // };
 
-    println!("Number: {}",number);
+    // println!("Number: {}",number);
+
+    let result = add_numbers(95, 67);
+    println!("{}",result);
+
+    // result = 89 + 67;
+    // println!("{}",result);
+
+}
+
+fn add_numbers(x: i32, y: i32) -> i32 {
+    let result = x + y;
+    if result > 200 {
+        return result - 200;
+    }
+    result
 }
